17defd70b80d02482e979620e543b5a64ee5ec14
LeeJun <ljun1118@ks.ac.kr>
2020-02-17 21:58:30 +0900

    20200217

---
+++ astar/Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+
+    private void Update()
+    {
+        float fx = Input.GetAxis("Horizontal");
+        float fy = Input.GetAxis("Vertical");
+
+        this.transform.Translate(
+            new Vector3(fx * Time.deltaTime * 5, 0, fy * Time.deltaTime * 5));
+    }
+}
